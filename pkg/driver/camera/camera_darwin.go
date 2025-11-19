6b8d83b86684ce702cff2d12f172d58d1ea50e47
hexbabe <sean.xiaochen.yu@gmail.com>
2025-11-19 13:21:03 -0500

    Remove unnecessary change in camera_darwin.go filtering

--- pkg/driver/camera/camera_darwin.go
+++ pkg/driver/camera/camera_darwin.go
@@ -22,11 +22,6 @@ func init() {
 
 // Initialize finds and registers camera devices. This is part of an experimental API.
 func Initialize() {
-	manager := driver.GetManager()
-	for _, d := range manager.Query(driver.FilterVideoRecorder()) {
-		manager.Delete(d.ID())
-	}
-
 	devices, err := avfoundation.Devices(avfoundation.Video)
 	if err != nil {
 		panic(err)
@@ -34,7 +29,7 @@ func Initialize() {
 
 	for _, device := range devices {
 		cam := newCamera(device)
-		manager.Register(cam, driver.Info{
+		driver.GetManager().Register(cam, driver.Info{
 			Label:      device.UID,
 			DeviceType: driver.Camera,
 			Name:       device.Name,
